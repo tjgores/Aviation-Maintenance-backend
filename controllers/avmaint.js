a204d2bbf9a97e826b3f528ce042f2ec707dbbdb
tomg <goresthomasj@johndeere.com>
2021-03-15 10:13:07 -0500

    changed avmaint to Avmaint in migrations and seeders, undid db migration and reseeded table

--- controllers/avmaint.js
+++ controllers/avmaint.js
@@ -4,7 +4,7 @@ const getAll = (req, res) => {
     console.log(Avmaint);
     Avmaint.findAll()
     .then(avmaints => {
-        console.log(avmaints)
+ //       console.log(avmaints)
 //      use the below line res.json(cities) to make your data work like an
 //      API built like project 2 except changing the way it works in
 //      you controller. This allows you to control the format of the API
--- seeders/20210310144024-avmaint.js
+++ seeders/20210310144024-avmaint.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('maintenance_dev', [{
+    await queryInterface.bulkInsert('Avmaints', [{
     
     acmodel: "A35",
     engmodel: "O-470",
